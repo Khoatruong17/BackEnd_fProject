56c1fc22c340dff2b0fa4acb394898d43b1da94f
Khoatruong17 <truongndkgch190486@fpt.edu.vn>
2024-10-02 18:06:35 +0700

    ok

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-const express = require('express');
+const express = require("express");
 const connection = require("./config/database");
 const apiRouter = require("./routers/apiRouter");
-const errorHandler = require('./middleware/errorHandler');
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
+const errorHandler = require("./middleware/errorHandler");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 const passport = require("passport");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-
-
 
 const app = express();
 const port = process.env.PORT || 10000;
@@ -18,23 +16,27 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(errorHandler);
 
-app.set('trust proxy', 1) // trust first proxy
-app.use(session({
-  secret: 'your-secret-key',
-  resave: false,
-  saveUninitialized: true,
-  store: MongoStore.create({ mongoUrl: process.env.DB_MONGOOSE, collectionName: " sessions" }),
-  cookie: { 
-    maxAge: 600000, // 10 minutes
-    secure: false, // set to true if your app is behind a reverse proxy (like a Nginx)
-    httpOnly: true, // prevent client-side JS from accessing the cookie
-    sameSite: true // recommended for secure cookies
-   }  
-}))
+app.set("trust proxy", 1); // trust first proxy
+app.use(
+  session({
+    secret: "your-secret-key",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.DB_MONGOOSE,
+      collectionName: " sessions",
+    }),
+    cookie: {
+      maxAge: 600000, // 10 minutes
+      secure: false, // set to true if your app is behind a reverse proxy (like a Nginx)
+      httpOnly: true, // prevent client-side JS from accessing the cookie
+      sameSite: true, // recommended for secure cookies
+    },
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
-
 
 // Initialize API routes
 apiRouter(app);
